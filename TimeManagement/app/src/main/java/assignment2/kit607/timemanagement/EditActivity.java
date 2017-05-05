d0fc4dcb700812518e9cfc1e8e42ec4637196785
San Wongwatthanaroek <san.air02@gmail.com>
2017-05-05 14:16:38 +1000

    Add some views

---
+++ TimeManagement/app/src/main/java/assignment2/kit607/timemanagement/EditActivity.java
@@ -0,0 +1,16 @@
+package assignment2.kit607.timemanagement;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Allen on 5/5/2017.
+ */
+
+public class EditActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_task);
+    }
+}
